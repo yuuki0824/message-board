57b9710bb67c5226f950ec167d04d39da93e2b51
yuukiito <rootxxx7@gmail.com>
2016-11-28 06:41:02 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,17 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @messages = Message.all  #Messageを全て取得する
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path, notice: 'メッセージを保存しました。'
+  end
+  
+  private
+    def message_params
+      params.require(:message).permit(:name, :body)
+    end
 end
